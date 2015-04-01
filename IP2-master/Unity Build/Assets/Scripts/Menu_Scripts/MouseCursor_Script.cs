2bb09106012a019b45ba401137521c00ded0a4fe
FarhadC <f.chamo@live.co.uk>
2015-04-01 20:30:44 +0100

    Implemented Main Menu and more work on Level 1
    Implemented Main Menu
    Created main menu scripts and animations

---
+++ IP2-master/Unity Build/Assets/Scripts/Menu_Scripts/MouseCursor_Script.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCursor_Script : MonoBehaviour {
+
+	public Texture2D cursorImage;
+
+	private int cursorWidth = 32;
+	private int cursorHeight = 32;
+
+	void Start()
+	{
+		Screen.showCursor = false;
+	}
+
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(Input.mousePosition.x, Screen.height - Input.mousePosition.y, cursorWidth, cursorHeight), cursorImage);
+	}
+}
---
+++ IP2-master/Unity Build/Assets/Scripts/Menu_Scripts/Quit_Button.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quit_Button : MonoBehaviour {
+
+	public Texture2D texture = null;
+	public Texture2D hoverTexture;
+	public Texture2D currentTexture;
+
+	public void Start()
+	{
+		currentTexture=texture;
+	}
+
+	IEnumerator PlayButtonClickPlay()
+	{
+		audio.Play();
+		yield return new WaitForSeconds(audio.clip.length);
+		Application.Quit();
+	}
+
+	private void OnGUI () {
+		if(GUI.Button (new Rect(Screen.width /4 , Screen.height *3 /6 , texture.width, texture.height), currentTexture, GUIStyle.none)) {
+
+			StartCoroutine(PlayButtonClickPlay());
+
+		}
+	}
+	public void Update() {
+		Rect boundingBox=new Rect(Screen.width /4 , Screen.height *3 /6 , currentTexture.width, currentTexture.height);
+		Vector3 actualMousePosition = Input.mousePosition;
+		actualMousePosition.y = Screen.height-Input.mousePosition.y;
+		if (boundingBox.Contains(actualMousePosition)){
+			//Input.mousePosition
+			currentTexture=hoverTexture;
+		}
+		else
+		{
+			currentTexture=texture;
+		}
+	}
+}
